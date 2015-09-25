623d55d66b81e766f352dbcfafd033bceb5a0d76
nishantcbse <nishantcbse@gmail.com>
2015-09-25 20:59:50 +0530

    loding,bugs fix

--- views/admin_snippets.php
+++ views/admin_snippets.php
@@ -1,3 +1,8 @@
+<?php
+$dir = plugin_dir_url( __FILE__ );
+?>
+<link rel="stylesheet" href="<?php echo $dir; ?>popup/dist/magnific-popup.css">
+<script src="<?php echo $dir; ?>popup/dist/jquery.magnific-popup.min.js"></script>
 
 <form method="post" action="" id="peecho-form">
     <?php wp_nonce_field('update_snippets', 'update_snippets_nonce'); ?>
@@ -46,7 +51,15 @@ if (!empty($snippets)) {
             <textarea name="<?php echo $key;
         ?>_snippet" class="large-text" style='width: 100%;' rows="5"><?php echo htmlspecialchars($snippet['snippet'], ENT_NOQUOTES);
         ?></textarea>
-       <div >Enter your Peecho button code here. You can find your Peecho button key on <a href="http://www.peecho.com/dashboard"  target="_blank">http://www.peecho.com/dashboard </a>, under Publications > Details > Print button</div> </td>
+        
+        <?php 
+			if(strlen($snippet['snippet']) > 0){
+				echo 'This is your Peecho button code. By adjusting the button code you can tweak the look and feel of your button. See our <a href="http://www.peecho.com/en/documentation/print-button" target="_blank">documentation</a> for details.';
+			}else{
+				echo 'Click Upload publication to start uploading a file to Peecho. Peecho will analyze the file and generate the button code for you.';
+			}
+		?>
+       </td>
        <td>
 	<?php       		
 		wp_enqueue_media();
@@ -76,7 +89,14 @@ if (!empty($snippets)) {
         ?>   
         </tbody>
     </table>
-    
+ <style>
+	.mfp-image-holder .mfp-close, .mfp-iframe-holder .mfp-close {
+		display: none !important;
+	}
+	img.mfp-img {
+		padding: 0px !important;
+	}	 
+ </style>  
     
 <script type="text/javascript">
 	function fileupload(id){
@@ -88,10 +108,20 @@ if (!empty($snippets)) {
 			var uploaded_image = image.state().get('selection').first();
 			var image_url = uploaded_image.toJSON().url;
 			jQuery('#image_url_'+id).val(image_url);
+			jQuery.magnificPopup.open({
+				items: {
+					src: '<?php echo $dir; ?>popup/ajax-loader.gif'
+				},
+				closeOnBgClick : false,
+				type: 'image'
+	
+			  // You may add options here, they're exactly the same as for $.fn.magnificPopup call
+			  // Note that some settings that rely on click event (like disableOn or midClick) will not work here
+			}, 0);
+				
 			setTimeout(function(){jQuery('#peecho-form').submit();},1000);
 		});
 	}
-	
 </script>   
     
 <input class="" type="hidden" value="Update Button" name="update-snippets">
@@ -107,3 +137,4 @@ if (!empty($snippets)) {
         }
         Peecho_Admin::submit('delete-snippets', __('Delete Selected', Peecho::TEXT_DOMAIN), 'button-secondary', false);
         echo '</form>';
+		
